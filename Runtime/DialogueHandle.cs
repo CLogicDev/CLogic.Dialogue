146606badd6320e0da3349b9f0849f13a2f5ba1e
Shaun Maurer Masson <shaun@clogic.dev>
2026-05-23 12:47:54 +0400

    feat: Playing dialogue returns a handle

---
+++ Runtime/DialogueHandle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CLogic.Dialogue
+{
+    [Serializable]
+    public struct DialogueHandle
+    {
+        private DialogueDirector director;
+
+        public DialogueGraph DialogueGraph { get; }
+        public bool IsPlaying { get; internal set; }
+        public bool IsFinished { get; internal set; }
+
+        public event Action OnFinish;
+        
+        internal DialogueHandle(DialogueDirector director, bool playState, DialogueGraph dialogueGraph, Action onFinish)
+        {
+            this.director = director;
+            IsPlaying = playState;
+            DialogueGraph = dialogueGraph;
+            IsFinished = false;
+            OnFinish = onFinish;
+        }
+        
+        internal void SetDialogueFinished()
+        {
+            IsFinished = true;
+            IsPlaying = false;
+            OnFinish?.Invoke();
+        }
+    }
+}
