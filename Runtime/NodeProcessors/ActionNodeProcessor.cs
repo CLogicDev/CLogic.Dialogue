f14cd3c11cffa69c790865a74634bb943abd8367
Shaun Maurer Masson <shaun@clogic.dev>
2026-05-27 09:27:34 +0400

    feat: Processor events use generic type as paramater

--- Runtime/NodeProcessors/ActionNodeProcessor.cs
+++ Runtime/NodeProcessors/ActionNodeProcessor.cs
@@ -13,15 +13,15 @@ namespace CLogic.Dialogue
         public Type NodeType => typeof(ActionNodeData);
         public bool CanProgressNode(DialogueNodeData dialogueNode, DialogueDirector director) => true;
         
-        public void ProcessNode(DialogueNodeData dialogueNode, DialogueDirector director)
+        public void ProcessNode(DialogueNodeData nodeDate, DialogueDirector director)
         {
-            var actionNodeData = (ActionNodeData)dialogueNode;
+            var actionNodeData = (ActionNodeData)nodeDate;
             
             foreach (BlockNodeData childBlock in actionNodeData.childBlocks)
             {
                 director.ProcessNode(childBlock, true);
             }
         }
-        public void HandleCancellation(DialogueNodeData dialogueNode, DialogueDirector director) {}
+        public void HandleCancellation(DialogueNodeData nodeDate, DialogueDirector director) {}
     }
 }
