25827e77b9fa456f5b49d1b159c9bfd3f6570f37
Shaun Maurer Masson <shaun@clogic.dev>
2026-07-15 20:23:35 +0400

    feat: Path visualization for local subgraphs

--- Runtime/DialogueDirector.Debugging.cs
+++ Runtime/DialogueDirector.Debugging.cs
@@ -7,12 +7,12 @@ namespace CLogic.Dialogue
 {
     public partial class DialogueDirector
     {
-        
         private Context currentContext;
         private DialogueNodeData previousNode;
         
         private void SetupDebugContext(DialogueGraph graph)
         {
+            Debug.Log("SetupDebugContext");
             currentContext?.Dispose();
             previousNode = null;
             currentContext = Registry.CreateVisualizationContext(graph.graphHash);
@@ -41,8 +41,30 @@ namespace CLogic.Dialogue
                 Integrations.LogWarning("Output port port is not valid to show execution path");
                 return;
             }
+            WireReference wire;
             
-            WireReference wire = currentContext.GetWireReference(outputPort, inputPort);
+            // Wire goes into a subgraph
+            if (CurrentDialogue.DialogueGraph.subgraphWireReferences.TryGetValue(inputPort, out DialogueGraph.SubgraphWireReference intoSubgraphWireRef))
+            {
+                WireReference parentGraphWire = currentContext.GetWireReference(outputPort, intoSubgraphWireRef.subgraphNodePort);
+                WireReference subgraphEntryWire = currentContext.GetWireReference(intoSubgraphWireRef.variableNodePort, inputPort);
+                
+                wire = parentGraphWire;
+                subgraphEntryWire.IsDashed = true;
+                currentContext.Motion.Play(subgraphEntryWire);
+            }
+            // Wire comes out from a subgraph
+            else if (CurrentDialogue.DialogueGraph.subgraphWireReferences.TryGetValue(outputPort, out DialogueGraph.SubgraphWireReference outOfSubgraphWireRef))
+            {
+                WireReference parentGraphWire = currentContext.GetWireReference(outOfSubgraphWireRef.subgraphNodePort, inputPort);
+                WireReference subgraphEntryWire = currentContext.GetWireReference(outputPort, outOfSubgraphWireRef.variableNodePort);
+                
+                wire = parentGraphWire;
+                subgraphEntryWire.IsDashed = true;
+                currentContext.Motion.Play(subgraphEntryWire);
+            }
+            else
+                wire = currentContext.GetWireReference(outputPort, inputPort);
             
             wire.IsDashed = true;
             currentContext.Motion.Play(wire);
